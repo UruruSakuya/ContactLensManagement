e8a480fe12f7493556555d6451b74182f3940cb2
UruruSakuya <atomic.transfer.comp@gmail.com>
2016-09-03 19:47:47 +0900

    Add Toast for displaying successful message.

--- app/src/main/java/jp/co/ur/nao/contactlensmanagement/AddingActivity.java
+++ app/src/main/java/jp/co/ur/nao/contactlensmanagement/AddingActivity.java
@@ -4,8 +4,10 @@ import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddingActivity extends AppCompatActivity {
 
@@ -16,7 +18,7 @@ public class AddingActivity extends AppCompatActivity {
     }
 
     public void addContactLens(View view) {
-        EditText contactLensNameView = (EditText)findViewById(R.id.contact_name);
+        EditText contactLensNameView = (EditText) findViewById(R.id.contact_name);
 
         DatabaseHelper databaseHelper = new DatabaseHelper(getBaseContext());
         SQLiteDatabase db = databaseHelper.getWritableDatabase();
@@ -32,5 +34,15 @@ public class AddingActivity extends AppCompatActivity {
         System.out.println(values.get(ContactMasterContract.ContactEntry.COLUMN_LENS_NAME));
         System.out.println(newRowId);
 
+        displayToast();
+        finish();
+
+    }
+
+    private void displayToast() {
+        Toast toast = Toast.makeText(this, R.string.info_success_adding_master, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+
+        toast.show();
     }
 }
