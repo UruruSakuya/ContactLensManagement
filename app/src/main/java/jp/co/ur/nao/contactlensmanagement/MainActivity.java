8369ea92828db70e5832800bd310a5e08f6e17cb
UruruSakuya <atomic.transfer.comp@gmail.com>
2016-09-03 19:50:09 +0900

    Remove unnecessary comment.

--- app/src/main/java/jp/co/ur/nao/contactlensmanagement/MainActivity.java
+++ app/src/main/java/jp/co/ur/nao/contactlensmanagement/MainActivity.java
@@ -16,48 +16,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    // Remove the below line after defining your own ad unit ID.
-//    private static final String TOAST_TEXT = "Test ads are being shown. "
-//            + "To show live ads, replace the ad unit ID in res/values/strings.xml with your own ad unit ID.";
-//
-//    private static final int START_LEVEL = 1;
-//    private int mLevel;
-//    private Button mNextLevelButton;
-//    private InterstitialAd mInterstitialAd;
-//    private TextView mLevelTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//
-//        // Create the next level button, which tries to show an interstitial when clicked.
-//        mNextLevelButton = ((Button) findViewById(R.id.next_level_button));
-//        mNextLevelButton.setEnabled(false);
-//        mNextLevelButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                showInterstitial();
-//            }
-//        });
-//
-//        // Create the text view to show the level number.
-//        mLevelTextView = (TextView) findViewById(R.id.level);
-//        mLevel = START_LEVEL;
 
         AdView adview = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         adview.loadAd(adRequest);
-
-        // Create the InterstitialAd and set the adUnitId (defined in values/strings.xml).
-//        mInterstitialAd = newInterstitialAd();
-//        loadInterstitial();
-//
-//        // Toasts the test ad message on the screen. Remove this after defining your own ad unit ID.
-//        Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_LONG).show();
     }
 
-    public  boolean addContact(MenuItem item) {
+    public boolean addContact(MenuItem item) {
         Intent intent = new Intent(this, AddingActivity.class);
         startActivity(intent);
 
@@ -68,16 +38,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         if (id == R.id.action_adding) {
@@ -85,54 +51,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    private InterstitialAd newInterstitialAd() {
-//        InterstitialAd interstitialAd = new InterstitialAd(this);
-//        interstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
-//        interstitialAd.setAdListener(new AdListener() {
-//            @Override
-//            public void onAdLoaded() {
-//                mNextLevelButton.setEnabled(true);
-//            }
-//
-//            @Override
-//            public void onAdFailedToLoad(int errorCode) {
-//                mNextLevelButton.setEnabled(true);
-//            }
-//
-//            @Override
-//            public void onAdClosed() {
-//                // Proceed to the next level.
-//                goToNextLevel();
-//            }
-//        });
-//        return interstitialAd;
-        return null;
-    }
-
-    private void showInterstitial() {
-        // Show the ad if it's ready. Otherwise toast and reload the ad.
-//        if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
-//            mInterstitialAd.show();
-//        } else {
-//            Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show();
-//            goToNextLevel();
-//        }
-    }
-
-    private void loadInterstitial() {
-        // Disable the next level button and load the ad.
-//        mNextLevelButton.setEnabled(false);
-//        AdRequest adRequest = new AdRequest.Builder()
-//                .setRequestAgent("android_studio:ad_template").build();
-//        mInterstitialAd.loadAd(adRequest);
-    }
-
-    private void goToNextLevel() {
-//        // Show the next level and reload the ad to prepare for the level after.
-//        mLevelTextView.setText("Level " + (++mLevel));
-//        mInterstitialAd = newInterstitialAd();
-//        loadInterstitial();
     }
 }
